a9604a76e0f77d4faf99a6b047201bf4220975b4
Fabrício Milanez <fabriciomilanez@protonmail.com>
2025-04-12 14:24:00 -0300

    [style] Prefer block body for methods with complex expressions

--- src/Catalog.Core/Repositories/EntityFrameworkCore/CategoryRepository.cs
+++ src/Catalog.Core/Repositories/EntityFrameworkCore/CategoryRepository.cs
@@ -9,8 +9,8 @@ using Microsoft.EntityFrameworkCore;
 namespace Catalog.Core.Repositories.EntityFrameworkCore;
 
 /// <summary>
-/// Implements <see cref="RepositoryBase{TEntity}"/>
-/// for the <see cref="Category"/> entity.
+/// Implements <see cref="RepositoryBase{TEntity}"/> for the
+/// <see cref="Category"/> entity.
 /// </summary>
 /// <remarks>
 /// Initializes a new instance of the <see cref="CategoryRepository"/> class.
@@ -27,20 +27,23 @@ public sealed class CategoryRepository(DbContext dbContext)
         uint limit = 10u,
         uint offset = 0u,
         CancellationToken cancellationToken = default
-    ) => await _dbSet.AsNoTracking()
-        .Join(
-            _dbContext.Set<Product>().AsNoTracking(),
-            category => category.Id, product => product.CategoryId,
-            (category, product) => new { Category = category, Product = product }
-        )
-        .Where(categoryProduct => (
-            categoryProduct.Category.Id == key
-            && !categoryProduct.Category.Hidden
-            && !categoryProduct.Product.Hidden
-        ))
-        .Select(categoryProduct => categoryProduct.Product)
-        .OrderBy(product => product.Id)
-        .Skip((int)offset)
-        .Take((int)limit)
-        .ToArrayAsync(cancellationToken);
+    )
+    {
+        return await _dbSet.AsNoTracking()
+            .Join(
+                _dbContext.Set<Product>().AsNoTracking(),
+                category => category.Id, product => product.CategoryId,
+                (category, product) => new { Category = category, Product = product }
+            )
+            .Where(categoryProduct => (
+                categoryProduct.Category.Id == key
+                && !categoryProduct.Category.Hidden
+                && !categoryProduct.Product.Hidden
+            ))
+            .Select(categoryProduct => categoryProduct.Product)
+            .OrderBy(product => product.Id)
+            .Skip((int)offset)
+            .Take((int)limit)
+            .ToArrayAsync(cancellationToken);
+    }
 }
