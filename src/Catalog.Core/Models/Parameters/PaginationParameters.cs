82ba1b0c5921e4059bb3f45b0e6812f86711d093
Fabrício Milanez <fabriciomilanez@protonmail.com>
2025-05-12 01:53:17 -0300

    Add deconstructuring functionality to `PaginationParameters`

--- src/Catalog.Core/Models/Parameters/PaginationParameters.cs
+++ src/Catalog.Core/Models/Parameters/PaginationParameters.cs
@@ -16,4 +16,20 @@ public partial record PaginationParameters
     [Required]
     [Range(minimum: PageSizeMinValue, maximum: PageSizeMaxValue)]
     public int PageSize { get; private init; }
+
+    /**
+     * Typically, this method would be generated automatically by the compiler
+     * for `record` types, however this is only the case when we provide a
+     * primary constructor, which are always public.
+     *
+     * This type implements a factory pattern to ensure its class invariant is
+     * not violated after instantiation so we can't actually provide any public
+     * constructors and thus, the `Deconstruct` method has to be implemened
+     * manually if we want this functionality.
+    */
+    public void Deconstruct(out int pageNumber, out int pageSize)
+    {
+        pageNumber = PageNumber;
+        pageSize = PageSize;
+    }
 }
